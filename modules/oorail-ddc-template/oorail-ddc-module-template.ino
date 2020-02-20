7d0bd57d0b4427f13d17ba0a6582128eee79b8ef
oorail <locoshed@oorail.com>
2020-02-20 02:58:07 -0500

    Added template module

--- modules/oorail-ddc-template/oorail-ddc-module-template.ino
+++ modules/oorail-ddc-template/oorail-ddc-module-template.ino
@@ -49,12 +49,20 @@
  *      Standard API endpoints
  *      HTTP-OTA Updates
  *      Supports ESP32 (ESP8266 is in progress)
- *      ESP32 Multi-Core Support
+ *      Basic ESP32 Multi-Core Support
  *      
  */
 
+/*
+ * The project, version and multicast DNS hostname should be changed to reflect the purpose of your module.
+ * You should also increment the OORAIL_FW_BUILD version with each build.
+ * 
+ */
+
 #define OORAIL_PROJECT "oorail-project"
+#define OORAIL_MDNS_HOSTNAME "oorail-template"
 #define OORAIL_VERSION "1.0.2"
+
 #define OORAIL_COPYRIGHT "Copyright (c) 2020 IdeaPier LLC (oorail.co.uk)"
 #define OORAIL_LICENSE "GPLv3"
 
@@ -86,8 +94,25 @@
 #if defined(ESP32)
 #define OORAIL_PLATFORM "ESP32"
 #define OORAIL_SERIAL_BAUD 115200
+
+/*
+ * FEATURE FLAGS:
+ * 
+ * OORAIL_OTA_UPDATE                Enable OTA Updates via HTTP (typically to controller)
+ * OORAIL_ESP32_MULTICORE           Enable Multi-Core Processing Support
+ * 
+ * OORAIL_FEATURE_ASYNC_WEB         Asynchronous Web (HTTP) Server
+ * OORAIL_FEATURE_LIST_API          Lists API Endpoints available via the serial monitor
+ * OORAIL_FEATURE_UPTIME            Handle Uptime
+ * 
+ */
 #define OORAIL_FEATURE_ASYNC_WEB 1
 #define OORAIL_OTA_UPDATE 1
+#define OORAIL_FEATURE_UPTIME 1
+/*
+ * #define OORAIL_ESP32_MULTICORE 1
+ * #define OORAIL_FEATURE_LIST_API 1
+ */
 #endif
 
 #if defined(ESP8266)
@@ -118,8 +143,6 @@
 #endif
 #endif
 
-#define OORAIL_MDNS_HOSTNAME "oorail-template"
-
 #if defined(OORAIL_FEATURE_ASYNC_WEB)
 #define OORAIL_HTTP_PORT 80
 
@@ -136,12 +159,14 @@ const char* oorail_ota_url = "http://oorail-controller.local/ota/firmware/oorail
 /* Global Variables */
 #if defined(ESP32)
 uint64_t chipid;
+#if defined(OORAIL_ESP32_MULTICORE)
+TaskHandle_t core_zero;
+#endif
 #endif
 
 #if defined(OORAIL_OTA_UPDATE)
 WiFiClient ota_client;
 #endif
-
 
 /* WiFi credentials */
 const char* ssid = "REPLACE-ME-WITH-YOUR-WIFI-SSID";
@@ -150,6 +175,11 @@ const char* password = "REPLACE-ME-WITH-YOUR-WIFI-PASSWORD";
 void setup() {
   Serial.begin (OORAIL_SERIAL_BAUD);
   oorail_banner();
+#if defined(ESP32)
+#if defined(OORAIL_ESP32_MULTICORE)
+  oorail_multicore_init();
+#endif
+#endif
   oorail_wifi();
 #if defined(OORAIL_FEATURE_ASYNC_WEB)
   oorail_web_init();
@@ -160,14 +190,92 @@ void setup() {
 void oorail_web_init_endpoints() {
 /*
  * server.on("/endpoint/path/", HTTP_METHOD, function); 
+ * 
+ * #if defined(OORAIL_FEATURE_LIST_API)
+ * Serial.printf(" * API Endpoint: /endpoint/path/ \r\n");
+ * #endif
+ * 
  */
   return;
 }
 #endif
 
+/*
+ * The loop function is continously executed on:
+ * 
+ *  ESP32             Core 1 (Dual-Core Platform)
+ *  ESP8266           Core 0 (Single Core Platform)
+ *  
+ */
 void loop() {
 
+#if !defined(OORAIL_ESP32_MULTICORE)
+/*
+ * When ESP32 multicore mode is disabled, we trigger core_zero_loop() at the bottom
+ * of the main loop function. This is done to help keep the code simplified. 
+ * 
+ * You may need to alter this behavior for your specific application.
+ * 
+ */
+  core_zero_loop();
+#endif 
+
 }
+
+/*
+ * The core_zero_loop() function is continously executed on:
+ * 
+ * ESP32              Core 0 (Dual-Core Platform)
+ * ESP8266            Core 0 (Single Core Platform)
+ * 
+ * Note: For ESP32 if MULTICORE is disabled, this runs on Core 1
+ *       When ESP32_MULTICORE is disabled, we don't need to fake the infinite loop.
+ *       You should also disable semaphore code when not using ESP32_MULTICORE as
+ *       you don't need to pass data between cores.
+ *       
+ * Tip:  Remember that both processor cores can access GLOBAL variables.
+ *       Make sure you put some kind of global variable lock on structures.
+ * 
+ */
+void core_zero_loop() {
+#if defined(OORAIL_ESP32_MULTICORE)
+  /* Arduino IDE does this for us in loop for core 1 but we need to fake an infinite loop for core 0 */
+  for (;;) {
+#endif
+
+/*
+ * ESP32: This is where you put code to execute on the other core
+ *        Remember that with Arduino IDE the ESP32 support runs other stuff on this core
+ *        So try not to overload it. If you disable multicore support this stuff gets executed
+ *        at the end of loop() unless you comment out the call to this function in loop().
+ *        
+ */
+
+     
+#if defined(OORAIL_ESP32_MULTICORE)
+  } /* This closes out the faked infinite loop eg. for (;;) { at the top of this function */
+#endif
+
+}
+
+#if defined(ESP32)
+#if defined(OORAIL_ESP32_MULTICORE)
+/*
+ * This function initializes a new task called corezero which invokes core_zero_loop
+ * 
+ */
+void oorail_multicore_init() {
+  xTaskCreatePinnedToCore(
+      core_zero_loop,                                   /* Function to implement the task */
+      "corezero",                                       /* Name of the task */
+      10000,                                            /* Stack size in words */
+      NULL,                                             /* Task input parameter */
+      0,                                                /* Priority of the task */
+      &core_zero,                                       /* Task handle. */
+      0);                                               /* Core where the task should run */
+}
+#endif
+#endif
 
 #if defined(OORAIL_FEATURE_ASYNC_WEB)
 void oorail_web_init() {
@@ -184,6 +292,9 @@ void oorail_web_init() {
  * 
  * /ota/update - Check for updates and perform OTA update
  */
+#if defined(OORAIL_FEATURE_LIST_API)
+  Serial.printf("\r\n * Registering API Endpoints: \r\n");
+#endif
   
   server.on("/", HTTP_GET, oorail_web_root);
   server.on("/debug", HTTP_GET, oorail_web_debug);
@@ -193,6 +304,18 @@ void oorail_web_init() {
   server.on("/status", HTTP_GET, oorail_web_status);
 #if defined(OORAIL_OTA_UPDATE)
   server.on("/ota/update", HTTP_GET, oorail_web_ota_update);
+#endif
+
+#if defined(OORAIL_FEATURE_LIST_API)
+  Serial.printf(" * API Endpoint: / \r\n");
+  Serial.printf(" * API Endpoint: /debug \r\n");
+  Serial.printf(" * API Endpoint: /hc \r\n");
+  Serial.printf(" * API Endpoint: /info \r\n");
+  Serial.printf(" * API Endpoint: /ping \r\n");
+  Serial.printf(" * API Endpoint: /status \r\n");
+#if defined(OORAIL_OTA_UPDATE)
+  Serial.printf(" * API Endpoint: /ota/update \r\n");
+#endif
 #endif
 
   oorail_web_init_endpoints();
@@ -241,18 +364,47 @@ void oorail_web_hc(AsyncWebServerRequest *request) {
 
 void oorail_web_info(AsyncWebServerRequest *request) {
   char response[1024];
-#if defined(ESP32)
+#if defined(OORAIL_FEATURE_UPTIME)
+  unsigned long uptime = millis();
+  unsigned long upseconds = 0;
+  unsigned long upminutes = 0;
+  unsigned long uphours = 0;
+  unsigned long updays = 0;
+#endif
   IPAddress oorail_ip = WiFi.localIP();
   IPAddress oorail_subnet = WiFi.subnetMask();
   IPAddress oorail_gw = WiFi.gatewayIP();
 
+#if defined(OORAIL_FEATURE_UPTIME)
+  upseconds = uptime/1000;
+  upminutes = upseconds/60; 
+  uphours = upminutes/60;
+  updays = uphours/24; 
+  upseconds -= (upminutes*60);
+  upminutes -= (uphours*60); 
+  uphours -= (updays*24); 
+#endif
+
+#if defined(ESP32)
+#if defined(OORAIL_FEATURE_UPTIME)
+  sprintf(response,"\r\n%s, version %s [%s-%04X%08X] (Build: %s)\r\n\r\nLicensed under %s\r\n\r\nModule IP: %d.%d.%d.%d (%d.%d.%d.%d)\r\nModule GW: %d.%d.%d.%d\r\n\r\nUptime is %d %s, %d %s, %d %s and %d %s\r\n\r\n",
+    OORAIL_PROJECT, OORAIL_VERSION, OORAIL_PLATFORM, (uint16_t)(chipid>>32), (uint32_t)chipid, OORAIL_FW_BUILD, OORAIL_LICENSE,
+    oorail_ip[0], oorail_ip[1], oorail_ip[2], oorail_ip[3], oorail_subnet[0], oorail_subnet[1], oorail_subnet[2], oorail_subnet[3],
+    oorail_gw[0], oorail_gw[1], oorail_gw[2], oorail_gw[3], updays,(updays > 1) ? "days" : (updays != 0) ? "day" : "days", uphours, (uphours > 1) ? "hours" : (uphours != 0) ? "hour" : "hours", 
+    upminutes, (upminutes > 1) ? "minutes" : (upminutes != 0) ? "minute" : "minutes", upseconds, (upseconds > 1) ? "seconds" : (upseconds != 0) ? "second" : "seconds");
+#endif
+#endif
+#if !defined(OORAIL_FEATURE_UPTIME)
+#if defined(ESP32)
   sprintf(response,"\r\n%s, version %s [%s-%04X%08X] (Build: %s)\r\n\r\nLicensed under %s\r\n\r\nModule IP: %d.%d.%d.%d (%d.%d.%d.%d)\r\nModule GW: %d.%d.%d.%d\r\n\r\n",
     OORAIL_PROJECT, OORAIL_VERSION, OORAIL_PLATFORM, (uint16_t)(chipid>>32), (uint32_t)chipid, OORAIL_FW_BUILD, OORAIL_LICENSE,
     oorail_ip[0], oorail_ip[1], oorail_ip[2], oorail_ip[3], oorail_subnet[0], oorail_subnet[1], oorail_subnet[2], oorail_subnet[3],
     oorail_gw[0], oorail_gw[1], oorail_gw[2], oorail_gw[3]);
-#else
+#endif
+#if !defined(ESP32)
   sprintf(response,"\r\n%s, version %s [%s] (Build: %s)\r\n\r\nLicensed under %s\r\n\r\n", 
     OORAIL_PROJECT, OORAIL_VERSION, OORAIL_PLATFORM, OORAIL_FW_BUILD, OORAIL_LICENSE);
+#endif
 #endif
   request->send(200, "text/plain", response);
 }
@@ -301,6 +453,7 @@ void oorail_web_ota_update(AsyncWebServerRequest *request) {
   }
 }
 #endif
+
 #endif
 
 void oorail_wifi() {
@@ -351,7 +504,20 @@ void oorail_banner() {
   Serial.print(" * ESP32 is using processor core ");
   Serial.println(xPortGetCoreID());
 #endif
+#if defined(OORAIL_FEATURE_ASYNC_WEB)
+  Serial.printf(" * Active Feature: Web Server (HTTP)\r\n");
+#endif 
 #if defined(OORAIL_OTA_UPDATE)
-  Serial.printf(" * OTA Update URL is %s\r\n\r\n ", oorail_ota_url);
+  Serial.printf(" * Active Feature: OTA HTTP Update\r\n");
+  Serial.printf(" * OTA Update URL is %s\r\n", oorail_ota_url);
 #endif
+#if defined(OORAIL_FEATURE_LIST_API)
+  Serial.printf(" * Active Feature: List API Endpoints on Serial Monitor\r\n");
+#endif
+#if defined(ESP32)
+#if defined(OORAIL_ESP32_MULTICORE)
+  Serial.printf(" * Active Feature: ESP32 Multicore Support Enabled\r\n");
+#endif
+#endif
+  Serial.printf("\r\n");
 }
